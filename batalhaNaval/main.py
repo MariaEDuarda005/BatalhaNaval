2b66f1808597dad2fd6b6fe3308419c726041aaa
MariaEDuarda005 <mariaesferreira05@gmail.com>
2023-12-21 11:15:15 -0300

    Third commit

--- batalhaNaval/main.py
+++ batalhaNaval/main.py
@@ -1,9 +1,11 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 import random
+import sys
 
 class telas:
     def __init__(self):
-        app = QtWidgets.QApplication([])
+
+        self.app = QtWidgets.QApplication([])
 
         self.telaInicial = uic.loadUi("telaInicial.ui")
         self.tabuleiro = uic.loadUi("tabuleiro.ui")
@@ -11,7 +13,11 @@ class telas:
         self.telaWinner = uic.loadUi("telaWinner.ui")
         self.telaInicial.show()
         self.telaInicial.play.clicked.connect(self.mudar_telaInicial)
-        self.vidas = 20
+        self.telaGameOver.btnExit.clicked.connect(self.Exit)
+        self.telaGameOver.btnAgain.clicked.connect(self.Again)
+        # self.vidas = 20
+        self.tentativas_max = 20
+        self.tentativas = 0
 
         coordenadasLetras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
         coordenadasNumeros = ["1","2","3","4","5","6","7","8","9","10"]
@@ -39,6 +45,7 @@ class telas:
 
         self.barcos = []
 
+        # laço para separar os caracteres letras de numeros de cada coordenada
         for a in range(len(sorteio)):
             if a == 0:
                 for i in range(len(sorteio)):
@@ -68,38 +75,67 @@ class telas:
                         self.barcos.append(f"{separado[0]}{int(separado[1])+(i+1)}")
                         print(self.barcos)
 
+        # comando para executar sempre que o botões da tela forem clicados
         for button in self.tabuleiro.findChildren(QtWidgets.QPushButton):
             button.clicked.connect(self.selecionarBotao)
             
-        app.exec()
+        self.app.exec() # executar o pyqt
+        sys.exit(self.app.exec_()) # acessa o sistema operacional e 'mata' o loop do pyqt
+
+    def restart(self):
+        QtCore.QCoreApplication.quit() #permite sair da aplicação
+        QtCore.QProcess.startDetached(sys.executable, sys.argv) # permite que o programa incie novamente antes de sair por completo da aplicação
 
     def mudar_telaInicial(self):
         self.telaInicial.close()
         self.tabuleiro.show()
+
+    def Exit (self):
+        self.telaGameOver.close()   
+
+    def Again (self):
+        self.restart()
     
     def selecionarBotao(self):
         sender = self.telaInicial.sender()
         senderCoordenada = sender.objectName()
     
+        # if senderCoordenada in self.barcos:
+        #     sender.setStyleSheet("background-image: url('img/bomba_estourou.png'); border: none")
+        #     self.barcos.remove(senderCoordenada)
+        #     self.vidas += 1
 
-        for i in range(1, 21, 1):
+        #     if len(self.barcos) == 0:
+        #         self.tabuleiro.close()
+        #         self.telaWinner.show()
+
+        # else:
+        #     sender.setStyleSheet("background-image: url('img/bomba.png'); border: none")
+        #     self.vidas -= 1
+
+        #     if self.vidas == 0:
+        #         self.tabuleiro.close()
+        #         self.telaGameOver.show()
+
+        if self.tentativas < self.tentativas_max:
+           
             if senderCoordenada in self.barcos:
                 sender.setStyleSheet("background-image: url('img/bomba_estourou.png'); border: none")
                 self.barcos.remove(senderCoordenada)
-
+               
                 if len(self.barcos) == 0:
                     self.tabuleiro.close()
                     self.telaWinner.show()
-
+                   
+                   
             else:
                 sender.setStyleSheet("background-image: url('img/bomba.png'); border: none")
-                self.vidas -= 1
-
-                if self.vidas == 0:
-                    self.tabuleiro.close()
-                    self.telaGameOver.show()
-
-
+                self.tentativas += 1
+                print(self.tentativas)
+ 
+        else:
+            self.tabuleiro.close()
+            self.telaGameOver.show()
 
 if __name__ == '__main__':
     c = telas()
